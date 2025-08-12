b0a6dd1ec4c802cda223ecd1ac40183c22565e9f
Mariam Mohamed Ossama <mariamosama772@gmail.com>
2025-08-12 19:48:15 +0300

    copy all.min.css to /docs

---
+++ docs/js/main.js
@@ -0,0 +1,17 @@
+const navLinks = document.querySelectorAll('nav li');
+
+navLinks.forEach((navLink) => {
+    navLink.addEventListener('click', function(e) {
+        e.preventDefault();
+    });
+    navLinks.forEach(l => l.classList.remove('text-[#8B5E35]'));
+    this.classList.add('text-[#8B5E35]');
+    const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+})
